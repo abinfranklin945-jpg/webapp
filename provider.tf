28eb1bf700f2629aebdce6c91e1701f938f8c7f4
abinfranklin945-jpg <abinfranklin945@gmail.com>
2026-07-14 02:04:53 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,20 +1,14 @@
-# Configure the Azure provider, you can have many
-# if you use azurerm provider, it's source is hashicorp/azurerm
-# short for registry.terraform.io/hashicorp/azurerm
-
-
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.12.0"
+      version = "=4.1.0"
     }
   }
-
-  required_version = ">= 1.9.0"
 }
-# configures the provider
 
+# Configure the Microsoft Azure Provider
 provider "azurerm" {
+
   features {}
 }
